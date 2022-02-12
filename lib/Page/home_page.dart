b4beed5085fe6d78dcc1b06bfe1e025538bf7be3
Chamroeun Sithy <sithy042@gmail.com>
2022-02-12 11:38:52 +0700

    Update 12-Feb-22

--- lib/Page/home_page.dart
+++ lib/Page/home_page.dart
@@ -1,70 +1,201 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _buildAppbar,
       body: _buildBoby,
       bottomNavigationBar: _buildBottom,
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: const Icon(
-          Icons.add,
-          size: 35.0,
-        ),
+        child: const Icon(Icons.add, size: 35.0),
+        backgroundColor: const Color.fromRGBO(35, 97, 172, 1),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 
-  get _buildAppbar => AppBar(
-        backgroundColor: const Color.fromRGBO(35, 97, 172, 1),
-        title: Container(
-          alignment: Alignment.topLeft,
-          child: const Text(
-            'Good Morning Sithy !',
-            style: TextStyle(fontFamily: "BLACKSWORD.OTF"),
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.notifications_active_outlined,
-              size: 33.0,
-            ),
-          ),
-          const SizedBox(
-            width: 25.0,
-          ),
-          Container(
-            width: 38.0,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(color: Colors.white, width: 3.3),
-              image: const DecorationImage(
-                image: AssetImage("assets/IMG_1255.JPG"),
+  get _buildBoby => Stack(
+        children: [
+          _buildBackground(),
+          Positioned(
+            top: 50.0,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SizedBox(
+                height: 180.0,
+                // color: Colors.redAccent,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Good morning, Sithy !",
+                      style: TextStyle(
+                        fontSize: 26.0,
+                        fontFamily: 'BLACKSWORD.OTF',
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 15.0),
+                    Row(
+                      children: [
+                        Container(
+                          height: 14.0,
+                          width: 14.0,
+                          decoration: BoxDecoration(
+                              color: const Color.fromRGBO(18, 68, 129, 1),
+                              borderRadius: BorderRadius.circular(8)),
+                        ),
+                        const SizedBox(width: 5.0),
+                        Container(
+                          height: 14.0,
+                          width: 14.0,
+                          decoration: BoxDecoration(
+                              color: const Color.fromRGBO(18, 68, 129, 1),
+                              borderRadius: BorderRadius.circular(8)),
+                        ),
+                        const SizedBox(width: 5.0),
+                        Container(
+                          height: 14.0,
+                          width: 14.0,
+                          decoration: BoxDecoration(
+                              color: const Color.fromRGBO(18, 68, 129, 1),
+                              borderRadius: BorderRadius.circular(8)),
+                        ),
+                        const SizedBox(width: 5.0),
+                        Container(
+                          height: 14.0,
+                          width: 14.0,
+                          decoration: BoxDecoration(
+                              color: const Color.fromRGBO(18, 68, 129, 1),
+                              borderRadius: BorderRadius.circular(8)),
+                        ),
+                        const SizedBox(width: 5.0),
+                        Container(
+                          height: 14.0,
+                          width: 14.0,
+                          decoration: BoxDecoration(
+                              color: const Color.fromRGBO(18, 68, 129, 1),
+                              borderRadius: BorderRadius.circular(8)),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 45.0),
+                    Row(
+                      children: const [
+                        Text(
+                          "January, 2022",
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontFamily: "BELCEADR.TTF",
+                            color: Colors.white,
+                          ),
+                        ),
+                        Icon(Icons.expand_more, color: Colors.white),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-          const SizedBox(
-            width: 30.0,
+          Positioned(
+            top: 74.0,
+            right: 20.0,
+            child: Row(
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: const Icon(Icons.notifications_active_outlined,
+                      size: 35.0, color: Colors.white),
+                ),
+                const SizedBox(width: 20.0),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: 35.0,
+                    width: 35.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white, width: 3.0),
+                      image: const DecorationImage(
+                        image: AssetImage("assets/IMG_1255.JPG"),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 310.0,
+            left: 20.0,
+            child: Row(
+              children: [
+                const Text(
+                  "Daily-Cost",
+                  style: TextStyle(
+                      fontSize: 26.0,
+                      fontFamily: "BELCEADR.TTF",
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  width: 90.0,
+                ),
+                const Text(
+                  "Total ",
+                  style: TextStyle(
+                      fontSize: 26.0,
+                      fontFamily: "BELCEADR.TTF",
+                      fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  height: 40.0,
+                  width: 155.0,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(3, 3),
+                        blurRadius: 5.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       );
 
-  get _buildBoby => Stack(
-        children: const [
-          Image(
-            image: AssetImage('assets/Backgroun.jpg'),
-            fit: BoxFit.cover,
-            alignment: Alignment.topCenter,
+  Stack _buildBackground() {
+    return Stack(
+      children: [
+        const Image(
+          image: AssetImage('assets/Backgroun.jpg'),
+          fit: BoxFit.cover,
+        ),
+        Container(
+          height: 230.0,
+          color: const Color.fromRGBO(35, 97, 172, 1),
+        ),
+        Positioned(
+          top: 180.0,
+          child: ClipOval(
+            child: Container(
+              height: 100.0,
+              width: 428.0,
+              color: const Color.fromRGBO(35, 97, 172, 1),
+            ),
           ),
-        ],
-      );
+        ),
+      ],
+    );
+  }
 
   get _buildBottom => BottomAppBar(
         notchMargin: 10.0,
@@ -74,20 +205,30 @@ class HomePage extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.home,
-                size: 30.0,
-              ),
+            ButtonBar(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.home,
+                    size: 30.0,
+                  ),
+                ),
+                const Text("Home"),
+              ],
             ),
             const SizedBox(),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.assessment,
-                size: 30.0,
-              ),
+            ButtonBar(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.assessment,
+                    size: 30.0,
+                  ),
+                ),
+                const Text("Record"),
+              ],
             ),
           ],
         ),
